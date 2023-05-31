b383cfb5d190d7bb769755bbcdbf8d23d436a0d1
Michel Maillard <info@maillard.li>
2023-05-31 16:25:15 +0200

    Installation

---
+++ eleventy.config.js
@@ -0,0 +1,9 @@
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addPassthroughCopy("src/assets");
+    return {
+      dir: {
+        input: "src",
+        output: "_site",
+      },
+    };
+  };
